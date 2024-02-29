82cc73f93634c77f2765fe39a6646cf71b4d2167
cn-randy <randy@twohals.com>
2024-02-29 04:15:25 -0500

    create page stubs and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import About from "../pages/About.jsx";
+import Home from "../pages/Home.jsx";
+import Profile from "../pages/Profile.jsx";
+import Signin from "../pages/SignIn.jsx";
+import Signup from "../pages/Signup.jsx";
 
 export default function App() {
-  return <h1 className="text-red-700">App</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
