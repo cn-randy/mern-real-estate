4166dc412ae2f801e4b2622dd37ede46edbce871
cn-randy <randy@twohals.com>
2024-02-29 04:20:40 -0500

    moved pages folder to src foldr

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import About from "../pages/About.jsx";
-import Home from "../pages/Home.jsx";
-import Profile from "../pages/Profile.jsx";
-import Signin from "../pages/SignIn.jsx";
-import Signup from "../pages/Signup.jsx";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import Signin from "./pages/SignIn.jsx";
+import Signup from "./pages/Signup.jsx";
 
 export default function App() {
   return (
