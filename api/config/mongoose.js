e36565a8bcca394ef096f594a49e8db4451d7018
cn-randy <randy@twohals.com>
2024-02-29 06:41:59 -0500

    connect to database

---
+++ api/config/mongoose.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+/**
+ ** Connect to mongoDB
+ ** Connect to mongoDB Atlas if in production mode
+ ** otherwise connect to local mongoDB service.
+ *
+ * @returns {Promise<void>}
+ */
+export const connectDB = async function () {
+  const uri =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URI
+      : process.env.MONGO_LOCAL_URI;
+
+  const con = await mongoose.connect(uri);
+  console.log(`MongoDB connected with host: ${con?.connection?.host}`);
+};
--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/mongoose.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => console.log("Server listening on port 3000!!"));
+//* connect to database and spin up server
+try {
+  await connectDB();
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Server is listening on port ${port}`));
+} catch (err) {
+  console.error(
+    `Server shutting down. Could not connect to database due to error: ${err}`,
+  );
+  process.exit(1);
+}
